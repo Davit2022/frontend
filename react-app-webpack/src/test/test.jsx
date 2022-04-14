33f63b79708b14f8485ab658c2d5c910e185eb30
Khachik <you@example.com>
2022-04-14 22:19:56 +0400

    test

--- react-app-webpack/src/test/test.jsx
+++ react-app-webpack/src/test/test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 function Test() {
+  const age=50
   const name = useSelector((state) => state.user.name);
   return (
     <div className="test">
