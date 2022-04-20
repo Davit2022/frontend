8e16d115587f0494bf9add68c9bb0d6d9ee4bc2a
Heghine2021 <heghine.sarkisyan@realschool.am>
2022-04-20 17:15:38 +0400

    Change src/component/footer.js file

--- react-app-webpack/src/component/footer/footer.js
+++ react-app-webpack/src/component/footer/footer.js
@@ -6,8 +6,6 @@ import Socials from "./socials/socials";
 function App() {
   return (
     <>
-      {/* <h1>My App Component</h1> */}
-      {/* <Navbar /> */}
       <FooterContent />
       <Socials />
       <FooterBottom />
