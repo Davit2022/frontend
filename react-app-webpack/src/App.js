e887ac70c2afb8014dd39658d34ca41e07b8c2a9
Heghine2021 <heghine.sarkisyan@realschool.am>
2022-04-20 16:33:25 +0400

    Add my task footer component

--- react-app-webpack/src/App.js
+++ react-app-webpack/src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import Footer from "./component/footer/footer";
 
-
 function App() {
   return (
     <>
-      {/* <h1>My App Component</h1> */}
       <Footer />
     </>
   );
 }
-
 export default App
