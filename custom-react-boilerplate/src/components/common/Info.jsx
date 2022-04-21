ce741a2c31e5f4437b2fc9d1bea4f96126349479
Nvard <nvard.badalyan.im@gmail.com>
2022-04-21 17:22:47 +0400

    header changes

--- custom-react-boilerplate/src/components/common/Info.jsx
+++ custom-react-boilerplate/src/components/common/Info.jsx
@@ -8,17 +8,14 @@ function Info() {
     <div className="info-items">
       <div>
         <HiOutlineLocationMarker />
-        {" "}
         Address: RA, Gyumri, A. Manukyan 1
       </div>
       <div>
         <BsTelephone />
-        {" "}
         Phone: +374 55 51 01 05
       </div>
       <div>
         <FiMail />
-        {" "}
         E-mail: market@shop.am
       </div>
     </div>
