6dbb3a42dc05423c654af024e4d5add508ef792d
heghine <heghine.sarkisyan@realschool.am>
2022-04-21 18:17:16 +0400

    Footer final version

--- custom-react-boilerplate/src/components/footer/footerButtom/FooterButtom.jsx
+++ custom-react-boilerplate/src/components/footer/footerButtom/FooterButtom.jsx
@@ -1,122 +1,27 @@
 import React from "react";
-// import "../Footer.css";
+import "../Footer.css";
 
 function FooterBottom() {
   return (
-    <footer
-      className="Footer3"
-      style={{
-        position: "absolute",
-        bottom: 0,
-        background: "#6b5517",
-        width: "96.9%",
-        paddingTop: "4px",
-        paddingRight: "20px",
-        paddingBottom: "4px",
-        paddingLeft: "20px",
-        textAlign: "center",
-      }}
-    >
+    <footer className="Footer3">
       <div className="footer-bottom">
-        <p
-          style={{
-            float: "left",
-            fontSize: "14px",
-            wordSpacing: "2px",
-            textTransform: "capitalize",
-            color: "#ffffff",
-          }}
-        >
+        <p>
           copyright &copy;2022
-          <a
-            href="/"
-            style={{
-              color: "#ffffff",
-              fontSize: "16px",
-              textDecoration: "none",
-            }}
-          >
-            Heghine
-          </a>
+          <a href="/"> Heghine </a>
         </p>
-        <div
-          className="footer-menu"
-          style={{
-            float: "right",
-          }}
-        >
-          <ul
-            style={{
-              display: "flex",
-            }}
-          >
-            <li
-              key="1"
-              style={{
-                paddingRight: "10px",
-                display: "block",
-              }}
-            >
-              <a
-                href="/"
-                style={{
-                  color: "#ffffff",
-                  textDecoration: "none",
-                }}
-              >
-                Home
-              </a>
+        <div className="footer-menu">
+          <ul>
+            <li key="1">
+              <a href="/"> Home</a>
             </li>
-            <li
-              key="2"
-              style={{
-                paddingRight: "10px",
-                display: "block",
-              }}
-            >
-              <a
-                href="/fruits"
-                style={{
-                  color: "#ffffff",
-                  textDecoration: "none",
-                }}
-              >
-                Fruits
-              </a>
+            <li key="2">
+              <a href="/fruits">Fruits</a>
             </li>
-            <li
-              key="3"
-              style={{
-                paddingRight: "10px",
-                display: "block",
-              }}
-            >
-              <a
-                href="/vegitables"
-                style={{
-                  color: "#ffffff",
-                  textDecoration: "none",
-                }}
-              >
-                Vegitables
-              </a>
+            <li key="3">
+              <a href="/vegitables">Vegitables</a>
             </li>
-            <li
-              key="4"
-              style={{
-                paddingRight: "10px",
-                display: "block",
-              }}
-            >
-              <a
-                href="/eco"
-                style={{
-                  color: "#ffffff",
-                  textDecoration: "none",
-                }}
-              >
-                Eco
-              </a>
+            <li key="4">
+              <a href="/eco">Eco</a>
             </li>
           </ul>
         </div>
--- custom-react-boilerplate/src/components/footer/socials/Socials.jsx
+++ custom-react-boilerplate/src/components/footer/socials/Socials.jsx
@@ -6,139 +6,30 @@ import { ImTwitter } from "react-icons/im";
 
 function Socials() {
   return (
-    <footer
-      className="Footer2"
-      style={{
-        position: "absolute",
-        bottom: 0,
-        listStyle: "none",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        color: "rgb(255 255 255)",
-        top: "83%",
-        height: "5px",
-        marginTop: "1rem",
-        marginRight: "0px",
-        marginBottom: "1rem",
-        marginLeft: "0px",
-      }}
-    >
-      <ul
-        className="socials"
-        style={{
-          listStyle: "none",
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        <li
-          key="1"
-          style={{
-            marginTop: "0",
-            marginRight: "20px",
-            marginBottom: "0",
-            marginLeft: "160%",
-          }}
-        >
-          <a
-            href="/Facebook"
-            style={{
-              textDecoration: "none",
-              color: "#ffffff",
-              border: "1.1px solid #ffffff",
-              padding: "10px",
-              borderRadius: "25%",
-            }}
-          >
+    <footer className="Footer2">
+      <ul>
+        <li key="1">
+          <a href="/Facebook">
             <FaFacebook />
           </a>
         </li>
-        <li
-          key="2"
-          style={{
-            marginTop: "0",
-            marginRight: "20px",
-            marginBottom: "0",
-            marginLeft: "20",
-          }}
-        >
-          <a
-            href="/Twitter"
-            style={{
-              textDecoration: "none",
-              color: "#ffffff",
-              border: "1.1px solid #ffffff",
-              padding: "10px",
-              borderRadius: "25%",
-            }}
-          >
+        <li key="2">
+          <a href="/Twitter">
             <ImTwitter />
           </a>
         </li>
-        <li
-          key="3"
-          style={{
-            marginTop: "0",
-            marginRight: "20px",
-            marginBottom: "0",
-            marginLeft: "20",
-          }}
-        >
-          <a
-            href="/GooglePlus"
-            style={{
-              textDecoration: "none",
-              color: "#ffffff",
-              border: "1.1px solid #ffffff",
-              padding: "10px",
-              borderRadius: "25%",
-            }}
-          >
+        <li key="3">
+          <a href="/GooglePlus">
             <FaGooglePlus />
           </a>
         </li>
-        <li
-          key="4"
-          style={{
-            marginTop: "0",
-            marginRight: "20px",
-            marginBottom: "0",
-            marginLeft: "20",
-          }}
-        >
-          <a
-            href="/Youtube"
-            style={{
-              textDecoration: "none",
-              color: "#ffffff",
-              border: "1.1px solid #ffffff",
-              padding: "10px",
-              borderRadius: "25%",
-            }}
-          >
+        <li key="4">
+          <a href="/Youtube">
             <FaYoutube />
           </a>
         </li>
-        <li
-          key="5"
-          style={{
-            marginTop: "0",
-            marginRight: "20px",
-            marginBottom: "0",
-            marginLeft: "20",
-          }}
-        >
-          <a
-            href="/LinkedinIn"
-            style={{
-              textDecoration: "none",
-              color: "#ffffff",
-              border: "1.1px solid #ffffff",
-              padding: "10px",
-              borderRadius: "25%",
-            }}
-          >
+        <li key="5">
+          <a href="/LinkedinIn">
             <FaLinkedinIn />
           </a>
         </li>
