be95174f4783fdbbf1eef5146e625d0b7d3db28f
Nvard <nvard.badalyan.im@gmail.com>
2022-04-21 14:06:44 +0400

    added header

---
+++ custom-react-boilerplate/src/components/common/Info.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { BsTelephone } from "react-icons/bs";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { FiMail } from "react-icons/fi";
+
+function Info() {
+  return (
+    <div className="info-items">
+      <span>
+        <HiOutlineLocationMarker />
+        {" "}
+        Address: RA, Gyumri, A. Manukyan 1
+      </span>
+      <span>
+        <BsTelephone />
+        {" "}
+        Phone: +374 55 51 01 05
+      </span>
+      <span>
+        <FiMail />
+        {" "}
+        E-mail: market@shop.am
+      </span>
+    </div>
+  );
+}
+
+export default Info;
