48c056a25cd279243c6c3bcc6ea3be20be93614d
heghine <heghine.sarkisyan@realschool.am>
2022-04-22 17:02:49 +0400

    change social file

--- custom-react-boilerplate/src/components/footer/socials/Socials.jsx
+++ custom-react-boilerplate/src/components/footer/socials/Socials.jsx
@@ -1,43 +1,14 @@
 import React from "react";
-// import { Link } from "react-router-dom";
-import { SOCIAL } from "../../../constants/Socials";
+import { Link } from "react-router-dom";
+import { SOCIAL } from "../../../constants/socials";
 
 function Socials() {
   return (
     <footer className="footer2">
-      {/* <ul>
-        <li key="1">
-          <a href="/Facebook">
-            <FaFacebook />
-          </a>
-        </li>
-        <li key="2">
-          <a href="/Twitter">
-            <ImTwitter />
-          </a>
-        </li>
-        <li key="3">
-          <a href="/GooglePlus">
-            <FaGooglePlus />
-          </a>
-        </li>
-        <li key="4">
-          <a href="/Youtube">
-            <FaYoutube />
-          </a>
-        </li>
-        <li key="5">
-          <a href="/LinkedinIn">
-            <FaLinkedinIn />
-          </a>
-        </li>
-      </ul> */}
-
       {SOCIAL.map((social) => (
-        // <Link to={`${social.link}`} key={social.id} ison={social.className}>
-        //   {social.icon}
-        // </Link>
-        <i className={social.className} />
+        <Link to={`${social.link}`} key={social.id}>
+          <i className={social.className} />
+        </Link>
       ))}
     </footer>
   );
---
+++ custom-react-boilerplate/src/constants/socials.js
@@ -0,0 +1,34 @@
+// import {
+//   FaFacebook, FaGooglePlus, FaLinkedinIn, FaYoutube,
+// } from "react-icons/fa";
+// import { ImTwitter } from "react-icons/im";
+
+export const SOCIAL = [
+  {
+    id: 1,
+    className: "fa fa-facebook-square fa-1x",
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: 2,
+    className: "fa fa-twitter fa-1x",
+    link: "/twitter",
+  },
+  {
+    id: 3,
+    className: "fa fa-google-plus fa-1x",
+    link: "/googlePlus",
+  },
+  {
+    id: 4,
+    className: "fa fa-youtube-play fa-1x",
+    link: "/youtube",
+  },
+  {
+    id: 5,
+    className: "fa fa-linkedin fa-1x",
+    link: "/linkedinIn",
+  },
+];
+
+export default { SOCIAL };
