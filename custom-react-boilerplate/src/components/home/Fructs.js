13d7ec8b34b35b8f064daf46be66a04a39fd76ea
musho04 <mushegh.esayan.im@gmail.com>
2022-05-10 15:42:17 +0400

    messege

---
+++ custom-react-boilerplate/src/components/home/Fructs.js
@@ -0,0 +1,99 @@
+import image from "../../assets/apples.png";
+import imagekiwi from "../../assets/kiwi.png";
+import imagepear from "../../assets/pear.png";
+
+const Array = [
+  {
+    id: "1",
+    imageSrc: image,
+    price: "$5",
+    market: "Fruct Shop",
+    title: "Apples",
+    description: "Only natural praducts",
+    color: "red",
+  },
+  {
+    id: "2",
+    imageSrc: imagekiwi,
+    price: "$50",
+    market: "Fruct Shop",
+    title: "Kiwi",
+    description: "Only natural praducts",
+    color: "green",
+  },
+  {
+    id: "3",
+    imageSrc: imagepear,
+    price: "$25",
+    market: "Fruct Shop",
+    title: "Pear",
+    description: "Only natural praducts",
+    color: "orange",
+  },
+  {
+    id: "4",
+    imageSrc: imagepear,
+    price: "$25",
+    market: "Fruct Shop",
+    title: "Pear",
+    description: "Only natural praducts",
+    color: "orange",
+  },
+  {
+    id: "5",
+    imageSrc: imagepear,
+    price: "$25",
+    market: "Fruct Shop",
+    title: "Pear",
+    description: "Only natural praducts",
+    color: "orange",
+  },
+  {
+    id: "6",
+    imageSrc: imagepear,
+    price: "$25",
+    market: "Fruct Shop",
+    title: "Pear",
+    description: "Only natural praducts",
+    color: "orange",
+  },
+  {
+    id: "7",
+    imageSrc: imagepear,
+    price: "$25",
+    market: "Fruct Shop",
+    title: "Pear",
+    description: "Only natural praducts",
+    color: "orange",
+  },
+  {
+    id: "8",
+    imageSrc: imagepear,
+    price: "$25",
+    market: "Fruct Shop",
+    title: "Pear",
+    description: "Only natural praducts",
+    color: "orange",
+  },
+  {
+    id: "9",
+    imageSrc: imagepear,
+    price: "$25",
+    market: "Fruct Shop",
+    title: "Pear",
+    description: "Only natural praducts",
+    color: "orange",
+  },
+  {
+    id: "10",
+    imageSrc: imagepear,
+    price: "$25",
+    market: "Fruct Shop",
+    title: "Pear",
+    description: "Only natural praducts",
+    color: "orange",
+  },
+
+];
+
+export default Array;
